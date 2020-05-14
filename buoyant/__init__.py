c340eaf03b43b561d44d2039f48d4905aa171861
fitnr <fitnr@fakeisthenewreal>
2020-05-13 21:37:05 -0400

    v0.6.0

--- buoyant/__init__.py
+++ buoyant/__init__.py
@@ -12,6 +12,6 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-__version__ = "0.5.2"
+__version__ = "0.6.0"
 
 from .buoy import Buoy, Observation
